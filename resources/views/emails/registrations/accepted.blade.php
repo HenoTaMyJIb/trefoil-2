8e5ea7f7765e780e1b14fe81d915ef86707f7572
Aljona Orlova <aljona.orlova@bpw-consulting.com>
2018-10-01 19:25:53 +0300

    Small fixes

--- resources/views/emails/registrations/accepted.blade.php
+++ resources/views/emails/registrations/accepted.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Tere, {{$firstname}}!
 
-Ootame Teid "{{$group->name}}" rühma proovitrennile. Proovitrennide tunniplaani saate vaadata <a href="http://www.trefoil.ee/proovitrennide-tunniplaan">siit</a>.
+Ootame Teid "{{$group->name}}" rühma proovitrennile. Proovitrennide tunniplaani saate vaadata <a href="http://trefoil.ee/proovitrennide-tunniplaan">siit</a>.
 
 Riided: mugavad spordiriided (püksid, t-särk). Soovi korral võib jalga panna sokid.
 
@@ -13,7 +13,7 @@ Ootame Teid treeningutele!
 -----------------------------
 Здравствуйте!
 
-Ждем Вас на пробную тренировку в группу "{{$group->name}}". Расписание пробных тренировок можете посмотреть <a href="http://www.trefoil.ee/proovitrennide-tunniplaan">здесь</a>.
+Ждем Вас на пробную тренировку в группу "{{$group->name}}". Расписание пробных тренировок можете посмотреть <a href="http://trefoil.ee/proovitrennide-tunniplaan">здесь</a>.
 
 Приходите в удобной спортивной одежде (шорты, футболка). Обувь не обязательна, можно тренироваться в носочках.
 

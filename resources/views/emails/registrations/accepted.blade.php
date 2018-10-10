b24cddfa18269dbfc0fc068cf54eac14debdb5f9
Aljona Raja <aljona.raja@bpw-consulting.com>
2018-10-10 13:33:06 +0300

    Accepted email

--- resources/views/emails/registrations/accepted.blade.php
+++ resources/views/emails/registrations/accepted.blade.php
@@ -5,6 +5,8 @@ Ootame Teid "{{$group->name}}" rühma proovitrennile. Proovitrennide tunniplaani
 
 Riided: mugavad spordiriided (püksid, t-särk). Soovi korral võib jalga panna sokid.
 
+Proovitrenni hind on 7 euro, tasumine kohapeal. Kui te otsustate treeninguid jätkata ja sõlmite lepingu, siis proovitrenni hind on kuutasu sees.
+
 Küsimuste korral kirjutage {{$email}}.
 
 Ootame Teid treeningutele!
@@ -16,6 +18,8 @@ Ootame Teid treeningutele!
 Ждем Вас на пробную тренировку в группу "{{$group->name}}". Расписание пробных тренировок можете посмотреть <a href="http://trefoil.ee/proovitrennide-tunniplaan">здесь</a>.
 
 Приходите в удобной спортивной одежде (шорты, футболка). Обувь не обязательна, можно тренироваться в носочках.
+
+Стоимость пробной тренировки 7 евро, оплата на месте. Если Вы заключите договор, то пробная тренировка входит в стоимость месячной оплаты.
 
 Если у Вас есть вопросы пишите нам {{$email}}.
 
